b348ee59080473f6204de1ae6aae917833d3dd5e
ChingYing <a0952766976@gmail.com>
2017-01-08 17:47:19 +0800

    1.完成reduction.js 2.修改reduction.blade.php

--- public/js/reduction.js
+++ public/js/reduction.js
@@ -2,7 +2,7 @@
 * @Author: LihaoHuang
 * @Date:   2017-01-07 23:36:39
 * @Last Modified by:   LihaoHuang
-* @Last Modified time: 2017-01-08 00:02:48
+* @Last Modified time: 2017-01-08 17:33:51
 */
 
 'use strict';
@@ -12,36 +12,67 @@ var N2O_data = [41, 56, 25, 48, 72, 34, 20];
 //瀏覽器載入後執行
 window.onload=function() { 
 
-	var xmlFile="../xml/GreenhouseGasRpt.xml";
+	var xmlFile="http://www.dgbas.gov.tw/public/data/open/LocalStat/green10.xml";
 	//載入指定的xml檔案
     var xmlData=loadXMLFile(xmlFile);
 	//取得DOM節點內的值
 	var Data = xmlData.getElementsByTagName("Data");
-	// x.push(50);
+	//x.push(50);
+	console.log(Data);
+	console.log(Data[0].childNodes[1]);
+	console.log(Data[0].childNodes[17].innerHTML);
+	var Year = new Array();
+	var Dust = new Array();	//落塵量
+	var PM25 = new Array();
+	var SO2 = new Array();	//二氧化硫
+	var NO = new Array();  //一氧化氮
+	var NO2 = new Array();	//二氧化氮
+	var O3 = new Array();	//臭氧
 
-
-
-
-
+	for(var i = 0; i < Data.length; i++){
+		Year.push(Data[i].childNodes[1].innerHTML);
+		Dust.push(Data[i].childNodes[5].innerHTML);
+		PM25.push(Data[i].childNodes[9].innerHTML);
+		SO2.push(Data[i].childNodes[13].innerHTML);
+		NO.push(Data[i].childNodes[15].innerHTML);
+		NO2.push(Data[i].childNodes[17].innerHTML);
+		O3.push(Data[i].childNodes[19].innerHTML);
+	}
+	console.log(NO2);
 
 	// Bar chart
 	var ctx = document.getElementById("mybarChart");
 	var mybarChart = new Chart(ctx, {
 		type: 'bar',
 		data: {
-			labels: ["January", "February", "March", "April", "May", "June", "July"],
+			labels: Year,
 			datasets: [
 			{
-				label: 'CO2',
-				backgroundColor: "#26B99A",
-				data: [51, 30, 40, 28, 92, 50, 45]
+				label: '臭氧',
+				backgroundColor: "#ff00ff",
+				data: O3
 			},
 			{
-				label: 'N2O',
-				backgroundColor: "#03586A",
-				data: N2O_data
+				label: '二氧化氮',
+				backgroundColor: "#00cc00",
+				data: NO2
+			},
+			{
+				label: '落塵量',
+				backgroundColor: "#455C73",
+				data: Dust
+			},
+			
+			{
+				label: '二氧化硫',
+				backgroundColor: "#BDC3C7",
+				data: SO2
+			},
+			{
+				label: '一氧化氮',
+				backgroundColor: "#26B99A",
+				data: NO
 			}
-
 			]
 		},
 		options: {
--- resources/views/DataVisualization/reduction.blade.php
+++ resources/views/DataVisualization/reduction.blade.php
@@ -1,14 +1,16 @@
 @extends('layouts.master')
 
-@section('title', '溫室氣體排放量')
+@section('title', '溫室氣體')
 
 @section('content')
-
+<div class="x_panel">
+	<font size="10px;">溫室氣體</font>
+</div>
 <div class="x_panel">
 	<div class="col-md-12 col-sm-12 col-xs-12">
 		<div class="x_panel">
 			<div class="x_title">
-				<h2>Bar graph <small>Sessions</small></h2>
+				<h2>年度比較 <small>Bar graph</small></h2>
 				<div class="clearfix"></div>
 			</div>
 		<div class="x_content">
